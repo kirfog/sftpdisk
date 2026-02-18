436b02376a847fcb833eead995d7a8105d716e2b
kirfog <kirfog@gmail.com>
2026-02-18 15:43:30 +0300

    sftp

---
+++ disk.ps1
@@ -0,0 +1,13 @@
+$DriveLetter = "Z"
+$RemotePath = "\\sshfs\sftp@11.22.33.44!2222"
+
+if (Get-PSDrive -Name $DriveLetter -ErrorAction SilentlyContinue) {
+    Write-Host "unmount $DriveLetter"
+    $command = "net use $DriveLetter`: /delete /yes"
+    Invoke-Expression $command
+}
+else {
+    Write-Host "mount drive $DriveLetter to $RemotePath"
+    $command = "net use $DriveLetter`:` $RemotePath /persistent:no"
+    Invoke-Expression $command
+}
